651ffdb34987d021363bf1d98bfad4f9631983ad
Alessandro Vecchi <aleangel1212@gmail.com>
2020-04-27 18:30:14 -0600

    refactor: move text functions to text mod

---
+++ src/lib/text.rs
@@ -0,0 +1,32 @@
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::{Canvas, Texture, TextureCreator};
+use sdl2::ttf::Font;
+use sdl2::video::{Window, WindowContext};
+
+const FONT_WIDTH: u32 = 100;
+const FONT_HEIGHT: u32 = 200;
+const FONT_COLOR: Color = Color::RGB(255, 255, 255);
+
+pub fn create_texture_from_string<'a>(
+  texture_creator: &'a TextureCreator<WindowContext>,
+  font: Font,
+  string: String,
+) -> Texture<'a> {
+  let surface = font
+    .render(&string)
+    .blended(FONT_COLOR)
+    .expect("Unable to create font surface");
+
+  return texture_creator
+    .create_texture_from_surface(&surface)
+    .expect("Unable to create font texture");
+}
+
+pub fn render_string_texture(canvas: &mut Canvas<Window>, string: &Texture, position: (i32, i32)) {
+  let (x, y) = position;
+
+  canvas
+    .copy(&string, None, Rect::new(x, y, FONT_WIDTH, FONT_HEIGHT))
+    .expect("Unable to copy texture");
+}
